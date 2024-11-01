c92269f2d57c9b0b58194080f8fc1cb454350530
IVONIInnovation <info@ivoni.org>
2024-11-01 14:52:35 +0100

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import TimeWeatherApp from './components/TimeWeatherApp';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <TimeWeatherApp />
+  </React.StrictMode>
+);
