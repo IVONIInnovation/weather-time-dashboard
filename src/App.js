8448e206ecb9d61e1e9f2e65a3e02236fd612438
IVONIInnovation <info@ivoni.org>
2024-11-02 13:35:03 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,12 @@
+import React from 'react';
+import TimeWeatherApp from './components/TimeWeatherApp';
 
+function App() {
+  return (
+    <div className="App">
+      <TimeWeatherApp />
+    </div>
+  );
+}
+
+export default App;
